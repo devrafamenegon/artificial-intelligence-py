fbbfedbbead2ecebe21f9175bc9067f37685efc3
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-03-24 20:43:16 -0300

    feat: fixing exercise 2

--- ex02.py
+++ ex02.py
@@ -7,15 +7,14 @@
 # A VÁLVULA DE ENTRADA DE GÁS SOMENTE PODERÁ SER ABERTA SE O
 # DISPOSITIVO DE IGNIÇÃO DA CHAMA ESTIVER ATIVADO
 
-# SE O SENSOR DE TEMPERATURA MÁXIMA (TMAX ) FOR ATIVADO, A CHAMA
+# SE O SENSOR DE TEMPERATURA MÁXIMA (TMAX) FOR ATIVADO, A CHAMA
 # DEVERÁ SER DESLIGADA DESATIVANDO A VÁLVULA DE ENTRADA DE GÁS
 
-# SE O SENSOR DE PRESSÃO MÁXIMA (PMAX ) FOR ATIVADO, A CHAMA
+# SE O SENSOR DE PRESSÃO MÁXIMA (PMAX) FOR ATIVADO, A CHAMA
 # DEVERÁ SER DESLIGADA DESATIVANDO A VÁLVULA DE ENTRADA DE GÁS
 
 # A VÁLVULA DE CONTROLE DE SAÍDA DE VAPOR SOMENTE PODERÁ SER
-# ABERTA SE O SENSOR DE PRESSÃO OPERACIONAL ( ABERTA SE O SENSOR DE PRESSÃO OPERACIONAL (PressaoOperacional ) E O
-# SENSOR DE TEMPERATURA ( SENSOR DE TEMPERATURA (Toperacional) FOREM ATIVADOS
+# ABERTA SE O SENSOR DE PRESSÃO OPERACIONAL E O SENSOR DE TEMPERATURA FOREM ATIVADOS
 
 # EM CASO DE EMERGÊNCIA OPERACIONAL, DESLIGAR A ENTRADA DE
 # GÁS
@@ -33,32 +32,43 @@ from sklearn.neural_network import MLPClassifier
 # ENTRADAS
 #   ON/OFF  N1  N2  N3  TEMPERATURAMAX  PRESSAOMAX   PRESSAOOPERACIONAL  TEMPERATURAOPERACIONAL
 entrypoints = [
-    [1, 0, 0, 0, 0, 0, 0, 0],  # ON - Nenhum sensor ativado
-    [1, 1, 0, 0, 0, 0, 0, 0],  # ON - N1 ativado
-    [1, 1, 1, 0, 0, 0, 0, 0],  # ON - N1, N2 ativados
-    [1, 1, 1, 1, 0, 0, 0, 0],  # ON - N1, N2, N3 ativados
-    [1, 1, 1, 1, 1, 0, 0, 0],  # ON - N1, N2, N3, TMAX ativados
-    [1, 1, 1, 1, 0, 1, 1, 1],  # ON - N1, N2, N3, PMAX, POper, TOper ativados
-    [1, 1, 1, 1, 1, 0, 1, 1],  # ON - N1, N2, N3, TMAX, POper, TOper ativados
-    [0, 0, 0, 0, 0, 0, 0, 0],  # OFF - Nenhum sensor ativado
-    [0, 1, 0, 0, 0, 0, 0, 0],  # OFF - N1 ativado
-    [0, 1, 1, 0, 0, 0, 0, 0],  # OFF - N1, N2 ativados
-    [0, 1, 1, 1, 0, 0, 0, 0],  # OFF - N1, N2, N3 ativados
-    [0, 1, 1, 1, 1, 0, 0, 0],  # OFF - N1, N2, N3, TMAX ativados
-    [0, 1, 1, 1, 0, 1, 1, 1],  # OFF - N1, N2, N3, PMAX, POper, TOper ativados
-    [0, 1, 1, 1, 1, 0, 1, 1],  # OFF - N1, N2, N3, TMAX, POper, TOper ativados
+    [1., 0., 0., 0., 0., 0., 0., 0.],  # ON - Nenhum sensor ativado
+    [1., 1., 0., 0., 0., 0., 0., 0.],  # ON - N1 ativado
+    [1., 1., 1., 0., 0., 0., 0., 0.],  # ON - N1, N2 ativados
+    [1., 1., 1., 1., 0., 0., 0., 0.],  # ON - N1, N2, N3 ativados
+    [1., 1., 1., 0., 1., 0., 0., 0.],  # ON - N1, N2, TMAX ativados
+    [1., 1., 1., 0., 0., 1., 0., 0.],  # ON - N1, N2, PMAX ativados
+    [1., 1., 1., 0., 0., 1., 0., 0.],  # ON - N1, N2, TMAX e PMAX ativados
+    [1., 1., 1., 0., 0., 0., 1., 1.],  # ON - N1, N2, POper, TOper ativados
+    [0., 0., 0., 0., 0., 0., 0., 0.],  # OFF - Nenhum sensor ativado
+    [0., 1., 0., 0., 0., 0., 0., 0.],  # OFF - N1 ativado
+    [0., 1., 1., 0., 0., 0., 0., 0.],  # OFF - N1, N2 ativados
+    [0., 1., 1., 1., 0., 0., 0., 0.],  # OFF - N1, N2, N3 ativados
+    [0., 1., 1., 0., 1., 0., 0., 0.],  # OFF - N1, N2, TMAX ativados
+    [0., 1., 1., 0., 0., 1., 0., 0.],  # OFF - N1, N2, PMAX ativados
+    [0., 1., 1., 0., 0., 1., 0., 0.],  # OFF - N1, N2, TMAX e PMAX ativados
+    [0., 1., 1., 0., 0., 0., 1., 1.],  # OFF - N1, N2, POper, TOper ativados
 ]
 
 # SAÍDAS
-#   ON/OFF  VALVULACHAMA  ENTRADAAGUA  VALVULAGAS  SAIDAVAPOR  SAIDAAGUA
+#   VALVULACHAMA  ENTRADAAGUA  VALVULAGAS  SAIDAVAPOR  SAIDAAGUA
 expectedReturns = [
-    [0, 0, 0, 0, 1],  # Desligado
-    [1, 0, 0, 0, 1],  # Válvula de entrada de água aberta
-    [1, 1, 0, 0, 0],  # Válvula de entrada de água fechada, válvula de gás fechada
-    [1, 1, 1, 0, 0],  # Válvula de entrada de água fechada, válvula de gás aberta
-    [0, 0, 0, 0, 0],  # Desligado devido ao TMAX ativado
-    [0, 0, 0, 0, 0],  # Desligado devido ao PMAX ativado
-    [0, 0, 0, 0, 0],  # Desligado devido ao TMAX ativado
+    [0., 1., 0., 0., 0.],  # Válvula de entrada de água aberta
+    [0., 1., 0., 0., 1.],  # Válvula de entrada de água aberta
+    [1., 0., 1., 0., 0.],  # Válvula de chama e válvula de gás abertas
+    [0., 0., 0., 0., 1.],  # Saída de água aberta
+    [0., 0., 0., 0., 0.],  # Desligado devido ao TMAX ativado
+    [0., 0., 0., 0., 0.],  # Desligado devido ao PMAX ativado
+    [0., 0., 0., 0., 0.],  # Desligado devido ao TMAX e PMAX ativados
+    [1., 0., 1., 1., 0.],  # Válvula de chama, válvula de gás e saída de vapor abertas
+    [0., 0., 0., 1., 1.],  # Saída de água e de vapor abertas
+    [0., 0., 0., 1., 1.],  # Saída de água e de vapor abertas
+    [0., 0., 0., 1., 1.],  # Saída de água e de vapor abertas
+    [0., 0., 0., 1., 1.],  # Saída de água e de vapor abertas
+    [0., 0., 0., 1., 1.],  # Saída de água e de vapor abertas
+    [0., 0., 0., 1., 1.],  # Saída de água e de vapor abertas
+    [0., 0., 0., 1., 1.],  # Saída de água e de vapor abertas
+    [0., 0., 0., 1., 1.],  # Saída de água e de vapor abertas
 ]
 
 # CRIANDO A ESTRUTURA DA REDE
@@ -66,7 +76,7 @@ caldeira = MLPClassifier(
     solver="lbfgs",
     activation="logistic",
     alpha=1e-8,
-    hidden_layer_sizes=(450, 450),
+    hidden_layer_sizes=(100, 100),
     random_state=1,
 )
 
@@ -74,22 +84,22 @@ caldeira = MLPClassifier(
 caldeira.fit(entrypoints, expectedReturns)
 
 while True:
+    ON = int(input("Acionar botão ON? [0 - Não, 1 - Sim]: "))
     N1 = int(input("Digite o nível de água N1 (0 - desligado, 1 - ligado): "))
     N2 = int(input("Digite o nível de água N2 (0 - desligado, 1 - ligado): "))
     N3 = int(input("Digite o nível de água N3 (0 - desligado, 1 - ligado): "))
     TMAX = int(input("Sensor de temperatura máxima ativado? [0 - Não, 1 - Sim]: "))
     PMAX = int(input("Sensor de pressão máxima ativado? [0 - Não, 1 - Sim]: "))
-    PressaoOperacional = input(
-        "Sensor de pressão operacional ativado? [0 - Não, 1 - Sim]: "
+    PressaoOperacional = int(
+        input("Sensor de pressão operacional ativado? [0 - Não, 1 - Sim]: ")
     )
     TOperacional = int(
         input("Sensor de temperatura operacional ativado? [0 - Não, 1 - Sim]: ")
     )
-    ON = int(input("Acionar botão ON? [0 - Não, 1 - Sim]: "))
 
     # Realizar a previsão com base nas entradas
     predicted_output = caldeira.predict(
-        [[N1, N2, N3, TMAX, PMAX, PressaoOperacional, TOperacional, ON]]
+        [[ON, N1, N2, N3, TMAX, PMAX, PressaoOperacional, TOperacional]]
     )
 
     # Imprimir saídas previstas
